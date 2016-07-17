5700c7486a9689800aa8de1879eb75dd731abba0
Master-fd <asus@asusdeMac-Pro.local>
2016-07-17 17:03:28 +0800

    OK

---
+++ objc_msgForward/FDPerson.h
@@ -0,0 +1,13 @@
+//
+//  FDPerson.h
+//  objc_msgForward
+//
+//  Created by asus on 16/7/17.
+//  Copyright (c) 2016年 asus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FDPerson : NSObject
+
+@end
---
+++ objc_msgForward/FDPerson.m
@@ -0,0 +1,13 @@
+//
+//  FDPerson.m
+//  objc_msgForward
+//
+//  Created by asus on 16/7/17.
+//  Copyright (c) 2016年 asus. All rights reserved.
+//
+
+#import "FDPerson.h"
+
+@implementation FDPerson
+
+@end
